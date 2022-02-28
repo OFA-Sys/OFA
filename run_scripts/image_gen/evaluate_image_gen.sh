51fd9e3d2a2f4242db197265e69ab5be31e70f4d
Iurnem <menrui.mr@alibaba-inc.com>
2022-02-28 16:07:20 +0800

    update evaluate shell and add more comments.

--- run_scripts/image_gen/evaluate_image_gen.sh
+++ run_scripts/image_gen/evaluate_image_gen.sh
@@ -3,7 +3,11 @@
 user_dir=../../ofa_module
 bpe_dir=../../utils/BPE
 
-data=../../dataset/coco_image_gen_data/coco_vqgan_dev.tsv
+# It may take a long time for the full evaluation. You can sample a small split from the full_test split.
+# But please remember that you need at least thousands of images to compute FID and IS, otherwise the resulting scores
+# might also no longer correlate with visual quality.
+
+data=../../dataset/coco_image_gen_data/coco_vqgan_full_test.tsv
 path=../../checkpoints/image_gen_large_best.pt
 selected_cols=0,2,1
 split='test'
@@ -18,10 +22,10 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python ../../evaluate.py \
   --user-dir=${user_dir} \
   --task=image_gen \
   --batch-size=1 \
-  --log-format=simple --log-interval=10 \
-  --seed=7 \
+  --log-format=simple --log-interval=1 \
+  --seed=42 \
   --gen-subset=${split} \
-  --beam=16 \
+  --beam=24 \
   --min-len=1024 \
   --max-len-a=0 \
   --max-len-b=1024 \
@@ -33,9 +37,10 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python ../../evaluate.py \
   --num-workers=0 \
   --model-overrides="{\"data\":\"${data}\",\"bpe_dir\":\"${bpe_dir}\",\"selected_cols\":\"${selected_cols}\",\"clip_model_path\":\"${CLIP_MODEL_PATH}\",\"vqgan_model_path\":\"${VQGAN_MODEL_PATH}\",\"vqgan_config_path\":\"${VQGAN_CONFIG_PATH}\",\"gen_images_path\":\"${GEN_IMAGE_PATH}\"}"
 
-cd ./eval_utils
+# install requiremnts
+pip install scipy
 # compute IS
-python inception_score.py --gpu 4 --batch-size 128 --path1 ../${GEN_IMAGE_PATH}/top1
+python inception_score.py --gpu 4 --batch-size 128 --path1 ${GEN_IMAGE_PATH}/top1
 # compute FID, download statistics for test dataset first.
 wget https://ofa-beijing.oss-cn-beijing.aliyuncs.com/datasets/coco_image_gen_data/gt_stat.npz
-python fid_score.py --gpu 4 --batch-size 128 --path1 ../${GEN_IMAGE_PATH}/top1 --path2 ./gt_stat.npz
+python fid_score.py --gpu 4 --batch-size 128 --path1 ${GEN_IMAGE_PATH}/top1 --path2 ./gt_stat.npz
