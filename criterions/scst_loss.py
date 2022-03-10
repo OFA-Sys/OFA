59723e1d2031762e66acd82773dda8c931b419d4
Junyang Lin <justinlin930319@hotmail.com>
2022-03-10 22:40:16 +0800

    Update scst_loss.py

--- criterions/scst_loss.py
+++ criterions/scst_loss.py
@@ -1,7 +1,6 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
+# Copyright 2022 The OFA-Sys Team. 
+# All rights reserved.
+# This source code is licensed under the Apache 2.0 license found in the LICENSE file in the root directory.
 
 import math
 import string
