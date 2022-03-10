42344f687cdd11923ce69d006327e5cbe01784ba
Junyang Lin <justinlin930319@hotmail.com>
2022-03-10 23:15:06 +0800

    Update image_classify.py

--- tasks/cv_tasks/image_classify.py
+++ tasks/cv_tasks/image_classify.py
@@ -1,6 +1,7 @@
 # Copyright 2022 The OFA-Sys Team. 
 # All rights reserved.
-# This source code is licensed under the Apache 2.0 license found in the LICENSE file in the root directory.
+# This source code is licensed under the Apache 2.0 license 
+# found in the LICENSE file in the root directory.
 
 from dataclasses import dataclass, field
 import json
