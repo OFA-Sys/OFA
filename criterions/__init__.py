3d3270a1ecc26267f621fd69a9ad48c1ad9d94a3
Guangxiang Zhao <zhaoguangxiang@pku.edu.cn>
2022-03-10 16:11:47 +0800

    Add Encouraging Loss to OFA

--- criterions/__init__.py
+++ criterions/__init__.py
@@ -1,3 +1,4 @@
 from .scst_loss import ScstRewardCriterion
 from .label_smoothed_cross_entropy import AdjustLabelSmoothedCrossEntropyCriterion
 from .clip_scst_loss import ClipScstRewardCriterion
+from .label_smoothed_encouraging_loss import AjustLabelSmoothedEncouragingLossCriterion
