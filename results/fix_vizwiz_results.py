184d9bd80e755dcb553ffb6874c794ceb6088736
Velcorn <Velcorn@users.noreply.github.com>
2022-05-14 18:05:57 +0200

    Minor text fixes

--- results/fix_vizwiz_results.py
+++ results/fix_vizwiz_results.py
@@ -4,8 +4,8 @@ import os
 print('Fixing vizwiz results...')
 
 # Open results file and sort by question_id
-results_file = 'test_predict_vizwiz.json'
-with open(results_file, 'r') as f:
+results_file = 'test_predict'
+with open(f'{results_file}.json', 'r') as f:
     results = sorted(json.load(f), key=lambda x: x['question_id'])
 
 # Get image names
@@ -15,7 +15,7 @@ img_names = sorted(os.listdir('../dataset/vizwiz_data/test'))
 results = [{'image': img_name, 'answer': answer} for img_name, answer in zip(img_names, [r['answer'] for r in results])]
 
 # Save updated results
-with open('test_predict_vizwiz_fixed.json', 'w') as f:
+with open(f'{results_file}_fixed.json', 'w') as f:
     json.dump(results, f)
 
 print('Finished fixing vizwiz results!')
